2a263f938eba9332ed13dace093745efcc30d666
AakritiSedhai <secondshare007@gmail.com>
2022-07-01 19:56:59 +0545

    projectfiles

--- 2nd page.py
+++ 2nd page.py
@@ -11,7 +11,16 @@ img1 = ImageTk.PhotoImage(file="page2.png")
 my_canvas=Canvas(window,width=650,height=420,bg="#0a3570")
 my_canvas.create_image(0,0,image=img1,anchor=NW)
 my_canvas.pack(fill="both",expand=True)
-img2 = ImageTk.PhotoImage(Image.open("output-onlinepngtools.png"))
+img2 = ImageTk.PhotoImage(Image.open("output-onlinepngtools (1).png"))
+dashboard_label=Label(window,text="DASHBOARD",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
+dashboard_label.place(x=109,y=168)
+product_label=Label(window,text="PRODUCT",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
+product_label.place(x=423,y=168)
+lowstock_label=Label(window,text="LOW STOCK",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
+lowstock_label.place(x=111,y=275)
+transactions_label=Label(window,text="TRANSACTION",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
+transactions_label.place(x=395,y=274)
+
 
 
 b2 = Button(window, image=img2,
