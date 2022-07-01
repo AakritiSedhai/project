8730a1ba77c7bec6c8f30b0e100c8897ad7d0342
AakritiSedhai <secondshare007@gmail.com>
2022-07-01 19:27:49 +0545

    projectfiles

--- 2nd page.py
+++ 2nd page.py
@@ -2,14 +2,25 @@ from tkinter import *
 from PIL import Image, ImageTk
 window=Tk()
 window.title("Sign-in")
-window.geometry('650x420')
+window.geometry('630x420')
 window.resizable(False,False)
 window.config(bg="#0a3570")
-# image = Image.open("page2.png")
-# resize_image = image.resize((,420))
+image = Image.open("page2.png")
+resize_image = image.resize((650,420))
 img1 = ImageTk.PhotoImage(file="page2.png")
-my_canvas=Canvas(window,width=200,height=200,bg="#0a3570")
+my_canvas=Canvas(window,width=650,height=420,bg="#0a3570")
 my_canvas.create_image(0,0,image=img1,anchor=NW)
 my_canvas.pack(fill="both",expand=True)
+img2 = ImageTk.PhotoImage(Image.open("output-onlinepngtools.png"))
+
+
+b2 = Button(window, image=img2,
+
+            border=0)
+
+b2.place(x=10, y=8)
+
+
+
 
 window.mainloop()
