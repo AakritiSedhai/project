2d94c21ae71f940fed5b2e52ac5a6a696c5e10bb
AakritiSedhai <secondshare007@gmail.com>
2022-07-01 21:38:53 +0545

    changed

--- 2nd page.py
+++ 2nd page.py
@@ -12,24 +12,18 @@ my_canvas=Canvas(window,width=650,height=420,bg="#0a3570")
 my_canvas.create_image(0,0,image=img1,anchor=NW)
 my_canvas.pack(fill="both",expand=True)
 img2 = ImageTk.PhotoImage(Image.open("output-onlinepngtools (1).png"))
-dashboard_label=Label(window,text="DASHBOARD",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
-dashboard_label.place(x=109,y=168)
-product_label=Label(window,text="PRODUCT",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
-product_label.place(x=423,y=168)
-lowstock_label=Label(window,text="LOW STOCK",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
-lowstock_label.place(x=111,y=275)
-transactions_label=Label(window,text="TRANSACTION",font=("comic sans",20,"normal","underline"),bg="#fabd75",fg="#807772")
-transactions_label.place(x=395,y=274)
-
-
-
-b2 = Button(window, image=img2,
-
-            border=0)
-
+dashboard_btn=Button(window,text="DASHBOARD",font=("comic sans",18,"normal","underline"),
+                     bg="#fabd75",fg="#807772",border=0,activebackground="#fabd75",activeforeground="#807772")
+dashboard_btn.place(x=109,y=170)
+product_btn=Button(window,text="PRODUCT",font=("comic sans",18,"normal","underline"),
+                   bg="#fabd75",fg="#807772",border=0,activebackground="#fabd75",activeforeground="#807772")
+product_btn.place(x=423,y=170)
+low_stock_btn=Button(window,text="LOW STOCK",font=("comic sans",18,"normal","underline"),
+                     bg="#fabd75",fg="#807772",border=0,activebackground="#fabd75",activeforeground="#807772")
+low_stock_btn.place(x=111,y=277)
+transactions_btn=Button(window,text="TRANSACTION",font=("comic sans",18,"normal","underline"),
+                        bg="#fabd75",fg="#807772",border=0,activebackground="#fabd75",activeforeground="#807772")
+transactions_btn.place(x=398,y=277)
+b2 = Button(window, image=img2,border=0)
 b2.place(x=10, y=8)
-
-
-
-
 window.mainloop()
